2c784b671620dc843b674180c9226231fc7f923d
saif <saifalis@axioned.com>
2022-09-30 11:30:23 +0530

    image filter function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
-/* Author: 
+// Image Tab Filter function
+var filterList = document.querySelectorAll('.filter-list');
+var galleryList = document.querySelectorAll('.gallery-list figure');
 
-*/
+filterList.forEach(function (list) {
+  list.addEventListener('click',function () {
+    var activeList = document.querySelector('.active-list');
+    activeList.classList.remove('active-list');
+    list.classList.add('active-list');
+    listAttr = list.getAttribute('data-list');
+    galleryList.forEach(function (imgList) {
+      var imgAttr = imgList.getAttribute('data-img');
+      if (listAttr === imgAttr || listAttr === 'all') {
+        imgList.classList.remove('hide-content');
+      } else {
+        imgList.classList.add('hide-content');
+      }
+    })
+  })
+})
 
 
 

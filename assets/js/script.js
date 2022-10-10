216f91a503f380f0062b4c30f823f8ca3863fbbe
saif <saifalis@axioned.com>
2022-10-10 14:50:24 +0530

    escape key and slider function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,13 @@ var galleryImage = document.querySelectorAll('.gallery-list img');
 var modal = document.querySelector('.modalDiv');
 var modalImage = document.querySelector('.modalDiv img');
 var cancelBtn = document.querySelector('.cancel-btn a');
+var prevBtn = document.querySelector('.prevBtn a');
+var nextBtn = document.querySelector('.nextBtn a');
+var activeList = document.querySelector('.active-list');
+var currentIndex;
 
 filterList.forEach(function (list) {
   list.addEventListener('click', function () {
-    var activeList = document.querySelector('.active-list');
     activeList.classList.remove('active-list');
     list.classList.add('active-list');
     var listAttr = list.getAttribute('data-list');
@@ -18,28 +21,62 @@ filterList.forEach(function (list) {
         imgList.classList.remove('hide-content');
       } else {
         imgList.classList.add('hide-content');
-      }
+      };
     });
   });
 });
 
-// image function
-galleryImage.forEach(function (image) {
+// Image function
+galleryImage.forEach(function (image, i) {
+  image.index = i;
   image.addEventListener('click', function () {
+    currentIndex = image.index
     var imageSource = image.src;
     modalImage.src = imageSource;
     modal.classList.remove('hide-content');
     document.documentElement.classList.add('removeScroll');
+    image.parentElement.parentElement.classList.add('active-list');
   });
 });
 
-// modal function
-modal.addEventListener('click',function (e) {
-  if (e.target == modal || e.target == modal.children[0].children[0]) {
+// Modal function
+modal.addEventListener('click', function (e) {
+  if (e.target === modal || e.target === modal.children[0].children[0]) {
     modal.classList.add('hide-content');
     document.documentElement.classList.remove('removeScroll');
- }
+  };
 });
+
+// slider function
+nextBtn.addEventListener('click', function () {
+  currentIndex++;
+  if (currentIndex > galleryList.length - 1) {
+    currentIndex = 0;
+  }
+  slider();
+});
+prevBtn.addEventListener('click', function () {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = galleryList.length - 1;
+  }
+  slider();
+});
+function slider() {
+  activeList.classList.remove('active-list');
+  galleryList[currentIndex].classList.add('active-list');
+  modalImage.src = galleryImage[currentIndex].src;
+  console.log(modalImage);
+}
+
+// Escape key function
+document.addEventListener('keydown', function (e) {
+  if (e.key === "Escape") {
+    modal.classList.add('hide-content');
+    document.documentElement.classList.remove('removeScroll');
+  };
+});
+
 
 
 
